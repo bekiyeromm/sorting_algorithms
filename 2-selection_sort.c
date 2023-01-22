c022b424f3df604642b7c08ecd268b8bdcb9d9c7
bekiyeromm <bk3tena@gmail.com>
2023-01-22 11:50:20 -0800

    Write a function that sorts an array of integers in ascending order using the Selection sort algorithm
    Prototype: void selection_sort(int *array, size_t size);
    You’re expected to print the array after each time you swap two elements (See example below)
    Write in the file 2-O, the big O notations of the time complexity of the Selection sort algorithm, with 1 notation per line:
    
    in the best case
    in the average case
    in the worst case

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include"sort.h"
+/*void swap(int *a, int *b);*/
+/**
+ * selection_sort - sorts an element in acending order
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		min = i;
+		for (j = (i + 1); j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			swap(&array[min], &array[i]);
+			print_array(array, size);
+		}
+	}
+
+}
+/**
+ * swap - swaps the value
+ * @a: forst num to be swaped
+ * @b: second number to be swaped
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
