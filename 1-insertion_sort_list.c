7f1670aab13fdc7246924034c2ef95d9885bbb25
bekiyeromm <bk3tena@gmail.com>
2023-01-22 23:48:41 -0800

    Write a function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
    Prototype: void insertion_sort_list(listint_t **list);
    You are not allowed to modify the integer n of a node. You have to swap the nodes themselves.
    You’re expected to print the list after each time you swap two elements (See example below)
    Write in the file 1-O, the big O notations of the time complexity of the Insertion sort algorithm, with 1 notation per line:
    
    in the best case
    in the average case
    in the worst case

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * elsement in accending order using insertion sort
+ * @list: pointer to Dobule linked list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -26,6 +26,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+listint_t *swap_node(listint_t *node, listint_t **list);
 
 #endif
