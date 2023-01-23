eb65b2b8299412a16bbf2179ea621cf27d19863f
bekiyeromm <bk3tena@gmail.com>
2023-01-22 23:02:18 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,41 @@
-#include"sort.h"
-void swap(int *a, int *b);
+#include "sort.h"
+
 /**
- * selection_sort - sorts an element in acending order
- * @array: array to be sorted
- * @size: size of the array
- */
+* selection_sort- Implementing selection sort
+* @array: array to be sorted
+* @size: size of an array
+*/
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
+	size_t i, j, min_idx;
 
-	if (array == NULL || size < 2)
+	if (!array && size < 2)
 		return;
-	for (i = 0; i < (size - 1); i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
-		for (j = (i + 1); j < size; j++)
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
-			{
-				min = j;
-			}
+			if (array[j] < array[min_idx])
+				min_idx = j;
 		}
-		if (min != i)
+		if (min_idx != i)
 		{
-			swap(&array[min], &array[i]);
+			swap(&(array[i]), &(array[min_idx]));
 			print_array(array, size);
 		}
 	}
-
 }
+
 /**
- * swap - swaps the value
- * @a: forst num to be swaped
- * @b: second number to be swaped
- */
+* swap- Swapping two integers
+* @a: fisrt number to be swapped
+* @b: second number to be swapped
+*/
 void swap(int *a, int *b)
 {
-	size_t temp;
-
-	temp = *a;
+	int tmp = *a;
 	*a = *b;
-	*b = temp;
+	*b = tmp;
 }
