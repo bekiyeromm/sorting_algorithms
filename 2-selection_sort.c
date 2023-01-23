50b6c93ea6d52d80a2a1782218be22ca6c873f0a
bekiyeromm <bk3tena@gmail.com>
2023-01-22 23:20:47 -0800

    Write a function that sorts an array of integers in ascending order using the Selection sort algorithm
    Prototype: void selection_sort(int *array, size_t size);
    You’re expected to print the array after each time you swap two elements (See example below)
    Write in the file 2-O, the big O notations of the time complexity of the Selection sort algorithm, with 1 notation per line:
    
    in the best case
    in the average case
    in the worst case

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,41 +1,44 @@
-#include "sort.h"
-
+#include"sort.h"
+void swap(int *a, int *b);
 /**
-* selection_sort- Implementing selection sort
-* @array: array to be sorted
-* @size: size of an array
-*/
+ * selection_sort - sorts an element in acending order
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, j, min;
 
-	if (!array && size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < (size - 1); i++)
 	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
+		min = i;
+		for (j = (i + 1); j < size; j++)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
 		}
-		if (min_idx != i)
+		if (min != i)
 		{
-			swap(&(array[i]), &(array[min_idx]));
+			swap(&array[min], &array[i]);
 			print_array(array, size);
 		}
 	}
-}
 
+}
 /**
-* swap- Swapping two integers
-* @a: fisrt number to be swapped
-* @b: second number to be swapped
-*/
+ * swap - swaps the value
+ * @a: forst num to be swaped
+ * @b: second number to be swaped
+ */
 void swap(int *a, int *b)
 {
-	int tmp = *a;
+	size_t temp;
+
+	temp = *a;
 	*a = *b;
-	*b = tmp;
+	*b = temp;
 }
