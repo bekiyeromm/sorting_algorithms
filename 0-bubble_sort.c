752f3f06cd9da5c57149b109ef006b8f467ed163
bekiyeromm <bk3tena@gmail.com>
2023-01-22 21:51:26 -0800

    Write a function that sorts an array of integers in ascending order using the Bubble sort algorithm
    Prototype: void bubble_sort(int *array, size_t size);
    You’re expected to print the array after each time you swap two elements (See example below)
    Write in the file 0-O, the big O notations of the time complexity of the Bubble sort algorithm, with 1 notation per line:
    
    in the best case
    in the average case
    in the worst case

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+void swap(int *a, int *b);
 /**
  * bubble_sort - sort an array of integer in ascending order
  * @array: the array to order and print
@@ -8,27 +9,25 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i = 0, j = 0;
 
-	if (size <= 1)
+	if (size < 1 || array == NULL)
 		return;
 
 	for (i = 0; i < (size - 1); i++)
-		for (j = 1; j < size; j++)
-			if (array[j - 1] > array[j])
+		for (j = 0; j < (size - i - 1); j++)
+			if (array[j] > array[j + 1])
 			{
-				swap(&array[j - 1], &array[j]);
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 }
-
-/**
- * swap - swap value of two integer pointers
- * @a: first index int pointer
- * @b: first index +1 int pointer
- */
-void swap(int *a, int *b)
+/**                                                                                                    
+ * swap - swap value of two integer pointers                                                           
+ * @a: first index int pointer                                                                         
+ * @b: first index +1 int pointer                                                                      
+ */                                                                                                    
+void swap(int *a, int *b)                                                                              
 {
 	int temp;
-
 	temp = *a;
 	*a = *b;
 	*b = temp;
